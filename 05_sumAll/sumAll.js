c4d582caeb83ddf2006c2b90c6ae4c3fa649de3f
scrif <andrews.austin.94@gmail.com>
2022-04-17 17:34:44 -0500

    Remove curly bracket on line 4 that broke function

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -2,7 +2,6 @@ const sumAll = function(firstInt, secondInt) {
     if (!Number.isInteger(firstInt) || !Number.isInteger(secondInt)) return "Error";
     if (firstInt < 0 || secondInt <0) return "Error";
 
-    }
     let sum = 0;
     for (let i = firstInt; i < secondInt + 1; i++) {
         sum += i;
