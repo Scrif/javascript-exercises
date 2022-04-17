c222f51c5723499d064d61b3b49078a3b399f37b
scrif <andrews.austin.94@gmail.com>
2022-04-16 21:04:23 -0500

    Add function sumAll and some error logic
    Added if statement to check whether both values were integers.
    Added if statement to check if either integer was less than 0.
    Set sum = 0, added a for loop from firstInt to secondInt +1.
    For loop logic is sum += i.

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,12 @@
-const sumAll = function() {
+const sumAll = function(firstInt, secondInt) {
+    if (!Number.isInteger(firstInt) || !Number.isInteger(secondInt)) return "Error";
+    if (firstInt < 0 || secondInt <0) return "Error";
 
+    }
+    let sum = 0;
+    for (let i = firstInt; i < secondInt + 1; i++) {
+        sum += i;
+    }
 };
 
 // Do not edit below this line
