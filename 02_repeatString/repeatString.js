ac77b3fef8c001eefdfbaddf0ddf626c0e042493
scrif <andrews.austin.94@gmail.com>
2022-04-16 19:29:46 -0500

    Add function. Add for-loop inside function.
    Added two variables to repeatString: "word", "times".
    Set string initial value to ''.
    For loop takes the string and adds "inputs" to the string.

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,9 @@
-const repeatString = function() {
-
+const repeatString = function(inputs, quantity) {
+    if (times < 0) return "Error";
+    let string = '';
+    for (let i = 0; i < quantity; i++) {
+        string += inputs;
+    }
 };
 
 // Do not edit below this line
