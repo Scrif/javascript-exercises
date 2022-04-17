523d31162064c5195702cb1af57bb0b14cc45210
scrif <andrews.austin.94@gmail.com>
2022-04-17 17:41:40 -0500

    Add function to check whether firstInt > secondInt.
    Test failed when the first integer was greater than the second integer.
    Created a function to check for this case. It's currently empty.

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,9 @@
 const sumAll = function(firstInt, secondInt) {
     if (!Number.isInteger(firstInt) || !Number.isInteger(secondInt)) return "ERROR";
     if (firstInt < 0 || secondInt <0) return "ERROR";
+    if (firstInt > secondInt) {
+        
+    }
 
     let sum = 0;
     for (let i = firstInt; i < secondInt + 1; i++) {
