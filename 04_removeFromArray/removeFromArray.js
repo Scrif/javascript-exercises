84dcc8f9d83dd001775b2d07021f68127f6b9b15
scrif <andrews.austin.94@gmail.com>
2022-04-16 20:45:05 -0500

    Add array.forEach
    Added if statement looking at whether theArgs included the user-provided item.
    If it's included in theArgs. push it from newArray and return the newArray.

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,7 +1,12 @@
 const removeFromArray = function(...theArgs) {
     const newArray = [];
     const array = theArgs[0];
-
+    array.forEach((item) => {
+        if (!theArgs.includes(item)) {
+            newArray.push(item);
+        }
+    });
+    return newArray;
 };
 
 // Do not edit below this line
