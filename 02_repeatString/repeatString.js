c38eae3f59f80b9d835df9ea930575ac036320db
scrif <andrews.austin.94@gmail.com>
2022-04-16 19:43:40 -0500

    Fix Error message
    Changed "times" to "quantity" to match function input variable.

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,9 +1,10 @@
 const repeatString = function(inputs, quantity) {
-    if (times < 0) return "Error";
+    if (quantity < 0) return "Error";
     let string = '';
     for (let i = 0; i < quantity; i++) {
         string += inputs;
     }
+    return string;
 };
 
 // Do not edit below this line
