ef4be29796878bfa3957ac370574441fa7adb0ec
scrif <andrews.austin.94@gmail.com>
2022-04-18 16:20:06 -0500

    Add comments to getAge function

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -7,12 +7,14 @@ const findTheOldest = function(array) {
     }
 };
 
+    // If person isn't dead (has no deathDate), figure out what today's date
+    // is, and calculate their current age.
 const getAge = function(birthDate,deathDate) {
     if (!deathDate) {
         deathDate = new Date().getFullYear();
     }
     return deathDate - birthDate;
-}
+};
 
 // Do not edit below this line
 module.exports = findTheOldest;
