0fc353de6dbce5c9c1eb498e4d61bd68abc6a0dc
scrif <andrews.austin.94@gmail.com>
2022-04-18 16:16:46 -0500

    Add getAge function
    getAge takes two parameters, birthDate and deathDate, and checks to see if they're still
    alive. If they are, calculates person's current age.

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,10 +1,18 @@
 const findTheOldest = function(array) {
     return array.reduce(oldest, currentValue) => {
-        
-
-
+    const oldestAge = getAge(oldest.yearOfBirth, oldest.yearOfDeath); 
+    // Gets the   
+    const currentAge = getAge(currentValue.yearOfBirth, currentValue.yearOfDeath);
+    return oldestAge
     }
 };
+
+const getAge = function(birthDate,deathDate) {
+    if (!deathDate) {
+        deathDate = new Date().getFullYear();
+    }
+    return deathDate - birthDate;
+}
 
 // Do not edit below this line
 module.exports = findTheOldest;
