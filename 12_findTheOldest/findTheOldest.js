80ee54a594d4d1728eeebc5f7b723205b21c2169
scrif <andrews.austin.94@gmail.com>
2022-04-18 16:35:42 -0500

    Add comments to findTheOldest function

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,14 +1,17 @@
 const findTheOldest = function(array) {
     return array.reduce(oldest, currentValue) => {
-    const oldestAge = getAge(oldest.yearOfBirth, oldest.yearOfDeath); 
-    // Gets the   
+    // get the age of the oldest person in the group.
+    const oldestAge = getAge(oldest.yearOfBirth, oldest.yearOfDeath);
+    // get the age of the current person you're looking at in the array.    
     const currentAge = getAge(currentValue.yearOfBirth, currentValue.yearOfDeath);
-    return oldestAge
+    // check to see if the currentAge is older than the oldestAge.
+    // if so, currentValue is now the oldest person and is the new oldestAge.
+    return oldestAge < currentAge ? currentValue : oldest;
     }
 };
 
-    // If person isn't dead (has no deathDate), figure out what today's date
-    // is, and calculate their current age.
+// If person isn't dead (has no deathDate), figure out what today's date
+// is, and calculate their current age.
 const getAge = function(birthDate,deathDate) {
     if (!deathDate) {
         deathDate = new Date().getFullYear();
