f3716292b435a174d8526f2efdd3e2437c3f708d
scrif <andrews.austin.94@gmail.com>
2022-04-18 12:19:17 -0500

    Changed RegEx. Ran & passed all tests.

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,8 @@
-const palindromes = function () {
-
+const palindromes = function (string) {
+    var re = /[^A-Za-z0-9]/g;
+    var lower = string.toLowerCase().replace(re, '');
+    var reverseString = lower.split('').reverse().join('');
+    return reverseString === lower;
 };
 
 // Do not edit below this line
