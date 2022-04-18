66dd0d27468981c7308f0b4a23e28ef50a258538
scrif <andrews.austin.94@gmail.com>
2022-04-18 12:28:07 -0500

    Add logic to function to return a requested Fibonacci number in the sequence
    Starts at 0,1 and adds the previous value to the current value to result in the new value.
    Increments this way until the input integer is reached.

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,12 @@
-const fibonacci = function() {
-
+const fibonacci = function(integer) {
+    let a = 0;
+    let b = 1;
+    for (let i = 1; i < integer; i++) {
+        const c = b;
+        b = a + b;
+        a = c;
+    }
+    return b;
 };
 
 // Do not edit below this line
