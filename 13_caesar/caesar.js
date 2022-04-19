8650409d0b4e56596785f4b253fc77368274f700
scrif <andrews.austin.94@gmail.com>
2022-04-19 16:27:21 -0500

    Built and tested initial caesar cipher functions

--- 13_caesar/caesar.js
+++ 13_caesar/caesar.js
@@ -1,5 +1,23 @@
-const caesar = function() {
+const caesar = function(string, number) {
+    return string
+        .split("")
+        .map(letter => shiftLetter(letter, number))
+        .join("");
+};
 
+const codeSet = code => (code <97 ? 65 : 97);
+
+const mod = (a, b) => (a % b + b) % b;
+
+const shiftLetter = (letter, number) => {
+    const code = letter.charCodeAt();
+
+    if ((code >= 65 && code <= 90) || (code >= 97 && code <= 122)) {
+        return String.fromCharCode(
+            mod(code + number - codeSet(code), 26) + codeSet(code)
+        );
+    }
+    return letter;
 };
 
 // Do not edit below this line
