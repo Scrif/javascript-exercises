54faacdefcb7b64df4e8494a96958130b3b33a7a
scrif <andrews.austin.94@gmail.com>
2022-04-19 08:17:23 -0500

    Fix parenthesis on line 2
    Added missing parenthesis on the array.reduce function on line 2.

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,13 +1,13 @@
 const findTheOldest = function(array) {
-    return array.reduce(oldest, currentValue) => {
+    return array.reduce((oldest, currentPerson) => {
     // get the age of the oldest person in the group.
     const oldestAge = getAge(oldest.yearOfBirth, oldest.yearOfDeath);
     // get the age of the current person you're looking at in the array.    
-    const currentAge = getAge(currentValue.yearOfBirth, currentValue.yearOfDeath);
+    const currentAge = getAge(currentPerson.yearOfBirth, currentPerson.yearOfDeath);
     // check to see if the currentAge is older than the oldestAge.
     // if so, currentValue is now the oldest person and is the new oldestAge.
-    return oldestAge < currentAge ? currentValue : oldest;
-    }
+    return oldestAge < currentAge ? currentPerson : oldest;
+    })
 };
 
 // If person isn't dead (has no deathDate), figure out what today's date
